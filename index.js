ad2408536567552e4180187c764e433dda659007
Scott G <glover.scott92@gmail.com>
2020-06-04 19:10:24 -0700

    works need to add in GIF of running and a test

---
+++ index.js
@@ -0,0 +1,90 @@
+// requirements_________________________
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+const generateMarkdown = require("./utilities/generateMarkdown.js");
+
+// requirements_________________________
+
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "Give your project a title:"
+
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "Please Enter Your Name:",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your preferred e-mail address?",
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Enter your GitHub username:",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "please give a two sentance desciption of your project:"
+
+        },
+        {
+            type: "input",
+            name: "role",
+            message: "What is your role?",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is the purpose of your project?",
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "To install necessary dependencies, run the following command:",
+        },
+        {
+            type: "input",
+            name: "license",
+            message: "What licenses did you use??",
+        },
+        {
+            type: "input",
+            name: "languages",
+            message: "What languages are you using?",
+        },
+        {
+            type: "input",
+            name: "demo",
+            message: "Provide a Demo file URL:",
+        },
+        {
+            type: "input",
+            name: "test",
+            message: "To run tests, run which following command?:",
+        },
+
+        // ______________
+    ]);
+}
+
+promptUser()
+
+    .then((responses) => {
+        const read = generateMarkdown(responses);
+        return writeFileAsync("README.md", read);
+    })
+    .then(() => {
+        console.log("Successfully wrote to README.md");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
--- utilities/generateMarkdown.js
+++ utilities/generateMarkdown.js
@@ -1,4 +1,5 @@
-
+function generateMarkdown(data) {
+    return `
   ![profile picture](./assets/profilepicture1.jpg)
   ![GIT LOGO](./assets/git-sticker.jpg)
   ![GIT LOGO](./assets/gif)
@@ -13,28 +14,30 @@
   *Questions
   
     #Hi! My name is:
-    
+    ${ data.name}
     #Hi! My email is:
-    
+    ${ data.email}
     #Hi! My Github username is:
-    
+    ${ data.username}
     #project description:
-    
+    ${ data.description}
     #My role:
-    
+    ${ data.role}
     #My poject purpose
-    
+    ${ data.usage}
     #Necessary dependencies
-    
+    ${ data.installation}
     #Licenses used:
-    
+    ${ data.license}
     #Languages used:
-    
+    ${ data.languages}
     #DEMO URL:
-    
+    ${ data.demo}
     #Test Command:
+    ${ data.test}
     
     
     
-    
-    
+    `}
+
+module.exports = generateMarkdown;
